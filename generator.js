e044407f55dba4902d1854cd646773859fbea10d
Eric Stanulis <erstanulis@gmail.com>
2017-10-09 13:09:23 -0500

    Got a CLI working. Create and pull cards.

--- generator.js
+++ generator.js
@@ -11,10 +11,64 @@ inquirer.prompt([
         choices: ['Pull card', 'Create cloze card', 'Create basic card']
     }
 ]).then(function(answer){
-    console.log(answer);
+    //Switch statements are Bae.
     switch (answer.choice){
         case 'Pull card':
-        console.log("This actually worked!");
+        inquirer.prompt([
+        {
+            name: "cardChoice",
+            type: "list",
+            message: "Cool. Would you like a Cloze Card or a Basic Card?",
+            choices: ['Cloze Card', 'Basic Card']
+        }
+        ]).then(function(answer){
+            switch (answer.cardChoice){
+                case 'Basic Card':
+                fs.readFile("BasicCards.txt", "utf8", function(error, data){
+                    if (error){
+                        console.log(error);
+                    }
+                    basicCardArray = JSON.parse(data);
+                    randomNumber = Math.floor(Math.random() * basicCardArray.length);
+                    inquirer.prompt([
+                    {
+                        name: "card",
+                        type: "input",
+                        message: "Provide the answer: " + basicCardArray[randomNumber].front
+                    }
+                    ]).then(function(data){
+                        if (data.card.toLowerCase() == basicCardArray[randomNumber].back.toLowerCase()){
+                            console.log("Correct! The answer was " + basicCardArray[randomNumber].back);
+                        } else {
+                            console.log("I'm sorry. The correct answer was '" + basicCardArray[randomNumber].back + "'")
+                        }
+                    })
+                });
+                break;
+
+                case 'Cloze Card':
+                fs.readFile("ClozeCards.txt", "utf8", function(error, data){
+                    if (error){
+                        console.log(error);
+                    }
+                    clozeCardArray = JSON.parse(data);
+                    randomNumber = Math.floor(Math.random() * clozeCardArray.length);
+                    inquirer.prompt([
+                    {
+                        name: "card",
+                        type: "input",
+                        message: "Fill in the blank: " + clozeCardArray[randomNumber].partial
+                    }
+                    ]).then(function(data){
+                        if (data.card.toLowerCase() == clozeCardArray[randomNumber].cloze.toLowerCase()){
+                            console.log("Correct! The answer was " + clozeCardArray[randomNumber].fullText);
+                        } else {
+                            console.log("I'm sorry. The correct answer was '" + clozeCardArray[randomNumber].fullText + "'")
+                        }
+                    })
+                });
+            };
+        });
         break;
         case 'Create cloze card':
         inquirer.prompt([
@@ -49,5 +103,34 @@ inquirer.prompt([
         break;
         case 'Create basic card':
         console.log("Creating basic card");
+        inquirer.prompt([
+        {
+            name: "front",
+            type: "input",
+            message: "Ok. What would you like the front of the card to be?",
+        },
+        {
+            name: "back",
+            type: "input",
+            message: "Great. And what would you like the back of the card to be?"
+        }]).then(function(answer){
+            var newCard = new BasicCard(answer.front, answer.back);
+            var basicCardArray = [];
+            fs.readFile("BasicCards.txt", "utf8", function(error, data){
+                if (error){
+                    console.log(error);
+                }
+                basicCardArray = JSON.parse(data);
+                console.log(basicCardArray);
+                basicCardArray.push(newCard);
+                var stringified = JSON.stringify(basicCardArray);
+                fs.writeFile("BasicCards.txt", stringified, function(err, data){
+                    if (err){
+                        console.log("Error:");
+                        console.log(err);
+                    };
+                });
+            });
+        });
     }
 });
